6268a8bbd5230fabf2cfc7c73907f5470223d35d
Антон Русанов <a.rusanov86@gmail.com>
2025-05-06 18:36:34 +0300

    build for intel

--- cmake/SignMacApplications.cmake
+++ cmake/SignMacApplications.cmake
@@ -1,7 +1,3 @@
-# This script re-signs OpenMW.app and OpenMW-CS.app after CPack packages them. This is necessary because CPack modifies
-# the library references used by OpenMW to App relative paths, invalidating the code signature.
-
-# Obviously, we only need to run this on Apple targets.
 if (APPLE)
     set(OPENMW_APP "OpenMW")
     set(OPENMW_CS_APP "OpenMW-CS")
@@ -10,12 +6,20 @@ if (APPLE)
     foreach(app_name IN LISTS APPLICATIONS)
         set(FULL_APP_PATH "${CPACK_TEMPORARY_INSTALL_DIRECTORY}/ALL_IN_ONE/${app_name}.app")
         message(STATUS "Re-signing ${app_name}.app")
-        # Apple's codesign utility does not like directories with periods (.) in their names, so we'll remove it and
-        # create a symlink using the original name, which codesign is fine with.
+
+        # Handle osgPlugins directory
         file(GLOB OSG_PLUGINS_DIR "${FULL_APP_PATH}/Contents/PlugIns/osgPlugins*")
-        file(RENAME "${OSG_PLUGINS_DIR}" "${FULL_APP_PATH}/Contents/PlugIns/osgPlugins")
-        execute_process(COMMAND "ln" "-s" "osgPlugins" "${OSG_PLUGINS_DIR}"
-                        WORKING_DIRECTORY "${FULL_APP_PATH}/Contents/PlugIns/")
-        execute_process(COMMAND "codesign" "--force" "--deep" "-s" "-" "${FULL_APP_PATH}")
+        if (EXISTS "${OSG_PLUGINS_DIR}")
+            file(RENAME "${OSG_PLUGINS_DIR}" "${FULL_APP_PATH}/Contents/PlugIns/osgPlugins")
+            execute_process(COMMAND "ln" "-s" "osgPlugins" "${OSG_PLUGINS_DIR}"
+                            WORKING_DIRECTORY "${FULL_APP_PATH}/Contents/PlugIns/")
+        endif()
+
+        # Code signing
+        execute_process(COMMAND "codesign" "--force" "--deep" "-s" "-" "${FULL_APP_PATH}"
+                        RESULT_VARIABLE CODE_SIGN_RESULT)
+        if (CODE_SIGN_RESULT)
+            message(FATAL_ERROR "Code signing failed for ${app_name}.app")
+        endif()
     endforeach(app_name)
 endif (APPLE)
